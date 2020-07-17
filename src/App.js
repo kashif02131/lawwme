01108bb86c833da9632ef892306c436a34cc3ca2
Kashif <kashif02131@gmail.com>
2020-07-17 15:21:47 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import cover from "./cover.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="maintext">
+      <h1>
+        We are coming up soon! Join us on{" "}
+        <a href="https://www.facebook.com/Lawwme-112493720539928">Facebook</a>
+      </h1>
+      <img class="cover" src={cover} alt="lawwme" />
     </div>
   );
 }
